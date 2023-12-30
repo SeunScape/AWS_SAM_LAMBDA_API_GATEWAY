16090809b3aab940ebe638e3436cf1372cbda7d7
Rix <blessingalabi03@gmail.com>
2023-12-31 00:04:28 +0100

    cypress test

---
+++ cypress/e2e/lambdaApiGateway.cy.js
@@ -0,0 +1,10 @@
+describe('API Gateway and Lambda Test', () => {
+  it('Successfully calls the API Gateway endpoint', () => {
+    cy.request({
+      method: 'GET',
+      url: 'https://seunalabi.com/', 
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
+});
